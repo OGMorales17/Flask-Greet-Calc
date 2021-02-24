d1dcde351adfe25df53aece45ea90e3f4efe7a5f
Orlando G Morales <chorly786@gmail.com>
2021-02-23 22:48:00 -0500

    Complete exercise with a file on terminal copy

---
+++ greet/app.py
@@ -0,0 +1,29 @@
+# Greet
+# In the greet folder, Make a simple Flask app that responds to these routes with simple text messages:
+
+# /welcome
+# Returns “welcome”
+# /welcome/home
+# Returns “welcome home”
+# /welcome/back
+# Return “welcome back”
+# Once you’ve finished this, run the tests for it:
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/welcome')
+def welcome():
+    return 'welcome'
+
+
+@app.route('/welcome/home')
+def welcome_home():
+    return 'welcome home'
+
+
+@app.route('/welcome/back')
+def welcome_back():
+    return 'welcome back'
